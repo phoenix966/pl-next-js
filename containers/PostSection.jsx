9f5a08538c2624c28fe296606a68482c4df9edda
Uri Dilewsky <stranger354999@gmail.com>
2021-06-03 11:56:47 +0600

    test

--- containers/PostSection.jsx
+++ containers/PostSection.jsx
@@ -1,18 +1,11 @@
-import UnderlineTitle from '../components/UnderlineTitle'
-import Button from '../components/Button'
-import style from '../styles/post-section.module.css'
-import Post from '../components/Post'
-import {useState} from 'react';
+import UnderlineTitle from '../components/UnderlineTitle';
+import Button from '../components/Button';
+import style from '../styles/post-section.module.css';
+import Post from '../components/Post';
+import fetch from 'isomorphic-unfetch';
 
-export default function PostSection() {
-  const [post,setPost] = useState([
-    {imgSrc:'post/post.jpg',title:'Веб программирование',user:'2454',clock:'12ч. 28м',rating:'3',key:1},
-    {imgSrc:'post/post2.jpg',title:'Специалист интернет технологий',user:'2454',clock:'12ч. 28м',rating:'3',key:2},
-    {imgSrc:'post/post3.jpg',title:'Веб программирование',user:'2454',clock:'12ч. 28м',rating:'3',key:3},
-    {imgSrc:'post/post4.jpg',title:'Веб программирование',user:'2454',clock:'12ч. 28м',rating:'3',key:4},
-    {imgSrc:'post/post5.jpg',title:'Веб программирование',user:'2454',clock:'12ч. 28м',rating:'3',key:5},
-    {imgSrc:'post/post6.jpg',title:'Веб программирование',user:'2454',clock:'12ч. 28м',rating:'3',key:6},
-  ])
+export default function PostSection(props) {
+  console.log(props.prof);
   return (
     <section className={style.postSection}>
       <div className={style.postSection__container}>
@@ -38,20 +31,30 @@ export default function PostSection() {
             />
           </div>
         </div>
-        <ul className={style.post__columns}>
-          {post.map((item)=>{
+        {/* <ul className={style.post__columns}>
+          {data.map((item)=>{
             return <Post 
-                      imgSrc={item.imgSrc}
-                      title={item.title}
-                      user={item.user}
-                      clock={item.clock}
-                      rating={item.rating}
-                      key={item.key}
+                      // imgSrc={item.imgSrc}
+                      title={item.prof.title}
+                      // user={item.people}
+                      // clock={item.hours}
+                      // // rating={item.rating}
+                      // key={item.id}
                     />
             })}
-        </ul>
+        </ul> */}
       </div>
       
     </section>
   )
+}
+
+export async function getServerSideProps(){
+  const res = await fetch('http://localhost:1337/professions');
+  const data = await res.json();
+  return{
+    props: {
+      prof:{data}
+    },
+  }
 }
